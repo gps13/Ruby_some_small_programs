fa833bcb3585f576ca0f099940c0ab08a7f127d9
gps13 <g.psistakis@gmail.com>
2013-08-13 15:19:30 +0300

    zipcode method - no refactor

---
+++ event_manager/lib/event_manager_old_v2.rb
@@ -0,0 +1,26 @@
+require "csv"
+puts "EventManager Initialized!"
+
+unless File.exist? "event_attendees.csv"
+	raise "no file found"
+end
+################
+def clean_zipcode(zipcode)
+	if zipcode.nil?
+		zipcode = "00000"
+	elsif zipcode.length < 5
+		zipcode = zipcode.rjust 5, "0"
+	elsif zipcode.length > 5
+		zipcode = zipcode[0..4]
+	else
+		zipcode		
+	end
+end
+################
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+
+contents.each do |row|
+	name = row[:first_name]
+	zipcode = clean_zipcode(row[:zipcode])
+	puts "#{name} #{zipcode}"
+end
