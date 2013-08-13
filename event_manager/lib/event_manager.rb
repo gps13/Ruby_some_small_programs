6f1b1f88bf6fd9555b7dbb996d47332a02646b43
gps13 <g.psistakis@gmail.com>
2013-08-13 14:59:03 +0300

    use of CSV lib

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,1 +1,12 @@
+require "csv"
 puts "EventManager Initialized!"
+
+unless File.exist? "event_attendees.csv"
+	raise "no file found"
+end
+################
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+
+contents.each do |row|
+	puts "#{row[:first_name]} #{row[:zipcode]}"
+end
