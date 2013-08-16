bd442d80d60aa584416f88c2ae72b2205fdf9414
gps13 <g.psistakis@gmail.com>
2013-08-16 15:08:30 +0300

    finished

---
+++ 23andMe_part5.rb
@@ -0,0 +1,17 @@
+require 'rest-client'
+require 'json'
+
+base_url = "https://api.23andme.com/1/demo"
+token = "fdab6a6892b198e40c2484bf2121f761"
+headers = {Authorization: "Bearer #{token}"}
+
+response = RestClient.get base_url + "/risks/", headers=headers
+data = JSON.load response
+
+risks = data[0]["risks"]
+risks.each { 
+	|risk|
+    # Fill out the comparison variable
+    comparison=risk["risk"]<risk["population_risk"]?'Lower':risk["risk"]>risk["population_risk"]?'Higher':'Same'
+    puts "#{risk["description"]}: #{comparison}"
+}
